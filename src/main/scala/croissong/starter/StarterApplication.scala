4f1ab9a9ce3380ebf7b3ec3ece4851509d94dfd9
Jan Möller <jan.moeller0@gmail.com>
2018-05-13 16:24:57 +0200

    Convert to scala

---
+++ src/main/scala/croissong/starter/StarterApplication.scala
@@ -0,0 +1,11 @@
+package croissong.starter
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class StarterApplication
+
+object StarterApplication extends App {
+    SpringApplication.run(classOf[StarterApplication])
+}
